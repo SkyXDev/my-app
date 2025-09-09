56d92ad3227f389ff0a16f23a13a28a80d232c92
Johannes <gestaltwebdesignstudio@gmail.com>
2025-09-09 20:15:54 +0800

    substitute apos

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -122,7 +122,7 @@ export default function AuthPage() {
 					>
 						{isSignUp
 						? "Already have an account? Sign in"
-						: "Don't have an account? Sign up"}
+						: "Don&apos;t have an account? Sign up"}
 					</button>
 				</div>
 			</div>
